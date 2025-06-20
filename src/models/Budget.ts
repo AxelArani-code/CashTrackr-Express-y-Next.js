bbf71cd95f07ed6fb149b6348a325d5bbfb6fdf2
Axel-Aranibar <axelaranibar3@gmail.com>
2025-06-20 09:44:52 -0300

    Creando el Modelo de Usuario

--- src/models/Budget.ts
+++ src/models/Budget.ts
@@ -1,15 +1,16 @@
-import { Table, Column, DataType, HasMany, BelongsTo, ForeignKey,  Model } from "sequelize-typescript";
+import { Table, Column, DataType, HasMany, BelongsTo, ForeignKey,  Model, AllowNull } from "sequelize-typescript";
 import Expense from "./Expense";
 
 @Table({
     tableName: 'budgets'
 })
 class Budget extends Model{
+    @AllowNull(false)
     @Column({
         type: DataType.STRING(100)
     })
     declare name:string
-
+ @AllowNull(false)
     @Column({
         type: DataType.STRING(100)
     })
