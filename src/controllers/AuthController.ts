bd81b99c10555c20ad4a0749ab5efb961a132f6a
Axel-Aranibar <axelaranibar3@gmail.com>
2025-06-23 09:22:43 -0300

    Registrando Usuario

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,10 +1,26 @@
 import type { Request, Response } from "express"
+import User from "../models/User"
 
 
 export class AuthController{
     static createAccount = async (req: Request, res: Response)=>{
-
-
+        const {email}= req.body
+        //Prevenir duplicados
+        const userExists = await User.findOne({where: {email}})
+        if(userExists){
+            const error =  new Error('El usuario ya existe')
+            res.status(409).json({error: error.message})
+             
+        }
+       
+        
+        try {
+            const user = new User(req.body)
+            await user.save()
+            res.json('Cuenta Creada Correctamente')
+        } catch (error) {
+            res.status(500).json({error:'Hubo un error'})
+        }
 
     }
 }
